0e9faa298c66a1f995f23fdad667c28f73437fdc
endyendy <41787965+endyendy@users.noreply.github.com>
2021-05-29 18:36:30 +0900

    Update connector.minimal.php

--- php/connector.minimal.php
+++ php/connector.minimal.php
@@ -149,7 +149,7 @@ $opts = array(
 		// Items volume
 		array(
 			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
-			'path'          => '/opt/app-root/src',                 // path to files (REQUIRED)
+			'path'          => '/',                 // path to files (REQUIRED)
 			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
 			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
