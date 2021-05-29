4b1570930a5cfd973f77b7cfe6049a2d1c50f665
endyendy <41787965+endyendy@users.noreply.github.com>
2021-05-29 18:41:14 +0900

    Update connector.minimal.php

--- php/connector.minimal.php
+++ php/connector.minimal.php
@@ -151,6 +151,7 @@ $opts = array(
 			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
 			'path'          => '/',                 // path to files (REQUIRED)
 			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
+			'statOwner'	=> 'True'
 			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
 			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
